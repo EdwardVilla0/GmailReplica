d00d568322ec8fa1083d7dbe8fe46c0290a62862
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-07 12:58:41 -0700

    styling the input for the search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Header.css'
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import { IconButton } from '@material-ui/core';
 
 function Header() {
@@ -14,7 +16,11 @@ function Header() {
                     src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
                     alt="" />
             </div>
-            <div className="header__middle"></div>
+            <div className="header__middle">
+                <SearchIcon />
+                <input placeholder="search mail" type="text" />
+                <ArrowDropDownIcon className="header__inputCaret" />
+            </div>
             <div className="header__right"></div>
         </div>
     )
