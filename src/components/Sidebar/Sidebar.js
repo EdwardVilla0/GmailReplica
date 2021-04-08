5919169637ca29c5129565ae6b0347f878deb5e0
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-08 13:29:20 -0700

    created the sidebar option and styling the component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,18 @@
 import { Button } from '@material-ui/core';
 import React from 'react'
 import AddIcon from '@material-ui/icons/Add'
+import InboxIcon from '@material-ui/icons/Inbox'
 import './Sidebar.css';
+import SidebarOption from '../SidebarOption/SidebarOption';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <Button startIcon={<AddIcon fontSize="large" />}>Compose</Button>
+            <Button
+                className="sidebar__compose"
+                startIcon={<AddIcon fontSize="large" />}>Compose</Button>
 
+            <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
         </div>
     )
 }
