1257b01d7d394e2b20b44e91d8b73b5fa7bf548e
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-08 15:28:49 -0700

    adding styling and fixing positioning of components

--- src/components/EmailList/EmailList.js
+++ src/components/EmailList/EmailList.js
@@ -6,8 +6,12 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import KeyboardHideIcon from '@material-ui/icons/KeyboardHide'
 import SettingsIcon from '@material-ui/icons/Settings'
+import InboxIcon from '@material-ui/icons/Inbox'
+import PeopleIcon from '@material-ui/icons/People'
+import LocalOfferIcon from '@material-ui/icons/LocalOffer'
 import React from 'react'
 import './EmailList.css'
+import Section from '../Section/Section'
 
 function EmailList() {
     return (
@@ -25,6 +29,12 @@ function EmailList() {
                     <IconButton><KeyboardHideIcon /></IconButton>
                     <IconButton><SettingsIcon /></IconButton>
                 </div>
+
+            </div>
+            <div className="emailList__sections">
+                <Section Icon={InboxIcon} title="primary" color="red" selected />
+                <Section Icon={PeopleIcon} title="Social" color="#1a73e8" />
+                <Section Icon={LocalOfferIcon} title="Promotion" color="green" />
             </div>
         </div>
     )
