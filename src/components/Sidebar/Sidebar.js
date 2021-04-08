780fa6d6eef91d5ee6777be3f25204dd78306c60
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-08 13:57:05 -0700

    styling the hover inbox

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,6 +2,7 @@ import { Button } from '@material-ui/core';
 import React from 'react'
 import AddIcon from '@material-ui/icons/Add'
 import InboxIcon from '@material-ui/icons/Inbox'
+import StarIcon from '@material-ui/icons/Star'
 import './Sidebar.css';
 import SidebarOption from '../SidebarOption/SidebarOption';
 
@@ -12,7 +13,8 @@ function Sidebar() {
                 className="sidebar__compose"
                 startIcon={<AddIcon fontSize="large" />}>Compose</Button>
 
-            <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+            <SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected={true} />
+            <SidebarOption Icon={StarIcon} title="Starred" number={54} />
         </div>
     )
 }
