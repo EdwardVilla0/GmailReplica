bf2b4d83c999e87e18b785b19015984046cab15c
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-13 15:14:51 -0700

    installing react hooks for project

--- src/components/SendMail/SendMail.js
+++ src/components/SendMail/SendMail.js
@@ -1,9 +1,11 @@
 import { Button } from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/Close'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import './SendMail.css'
 
 function SendMail() {
+    const { register, handleSubmit, watch, errors } = useForm()
     return (
         <div className="sendMail">
             <div className="sendMail__header">
@@ -11,9 +13,13 @@ function SendMail() {
                 <CloseIcon className="sendMail__close" />
             </div>
             <form>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+                <input name="to" placeholder="To:" type="text" />
+                <input name="subject" placeholder="Subject" type="text" />
+                <input
+                    name="message"
+                    placeholder="Messages"
+                    type="text"
+                    className="sendMail__message" />
                 <div className="sendMail__options">
                     <Button
                         className="sendMail__send"
