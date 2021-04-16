906cd1e5bcce48bec0cba8f9ca2c0c7e9e463b35
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-15 17:50:16 -0700

    using firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCJH88lyyhx7zZX1MKAE0rNas7yrgyLsjs",
+    authDomain: "replica-8b0fe.firebaseapp.com",
+    projectId: "replica-8b0fe",
+    storageBucket: "replica-8b0fe.appspot.com",
+    messagingSenderId: "1015679809113",
+    appId: "1:1015679809113:web:8c868add4287f2faa66348",
+    measurementId: "G-M82R79TN9Y"
+};
